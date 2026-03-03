c4b7f4f6d2b8f59c5f727e88f6f77eac25ff7609
Nanditasaravanan07 <144513305+nandhitasaravanan0701@users.noreply.github.com>
2026-03-03 11:23:08 +0530

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+    char *words[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            // Print the word from the array (index is i-1)
+            printf("%s\n", words[i - 1]);
+        } else if (i > 9) {
+            // Check if the number is even or odd
+            if (i % 2 == 0) {
+                printf("even\n");
+            } else {
+                printf("odd\n");
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+
+
